9d6e6473126200d2e28a92d25f6b8d531f536626
razor <razorsh4rk@protonmail.com>
2019-02-17 18:39:43 +0100

    onclick stuff

--- Duplicate addon/main.js
+++ Duplicate addon/main.js
@@ -1,4 +1,4 @@
-/*let body = document.getElementById('issue_body'),
+let body = document.getElementById('issue_body'),
 	ctx = document.getElementsByClassName('form-actions')[0],
 	footer = document.getElementsByClassName('timeline-comment-wrapper timeline-new-comment composer')[0],
 	resAdded = false
@@ -24,38 +24,6 @@ ctx.innerHTML += btnInject
 
 window.addEventListener('click', function(e){
 	if(e.target.id == 'duplicate-button'){
-		req()
+		window.open('http://localhost:8081/results?url=' + window.location + '&text=' + body.value)
 	}
 })
-
-function req(){
-	let b = body.value
-	if(b.length != 0){
-		fetch('http://localhost:8081/detect', {
-		        method: "POST",
-		        body: JSON.stringify({
-		        	"URL" : window.location,
-		        	"TEXT" : b
-		        })
-		    })
-			.then((resp) => resp.json())
-			.then((res) => {
-				console.log(res)
-				if(!resAdded)
-					footer.innerHTML += resultInject(res)
-				else
-					document.getElementById('duplicate-results')
-						.innerHTML = res.origin
-			})
-	}else{
-		if(!resAdded)
-			footer.innerHTML += 
-				resultInject('gimme something to work with')
-		else
-			document.getElementById('duplicate-results')
-				.innerHTML = 'gimme something to work with'
-	}
-}
-*/
-window.open('http://localhost:8081/results?url=https://github.com/mrdoob/three.js/issues/new&text=I%20dislike%20this%20one%20more%20then')
-//let a = browser.runtime.getURL('/test.html'))
